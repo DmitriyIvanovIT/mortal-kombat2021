4f0870c7933146918b9f6991ad910ca3370d6c30
Anastasia Gartseva <amgartseva@gmail.com>
2021-03-24 11:10:38 +0300

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,11 @@
-$(document).ready(function () {
-    $('#app').vide({
-        mp4: 'assets/video/video.mp4',
-        webm: 'assets/video/video.webm',
-        ogv: 'assets/video/video.ogv',
-        poster: 'assets/images/cover-image.jpg'
+if (window.screen.width > 576) {
+    $(document).ready(function () {
+        $('#app').vide({
+            mp4: 'assets/video/video.mp4',
+            webm: 'assets/video/video.webm',
+            ogv: 'assets/video/video.ogv',
+            poster: 'assets/images/cover-image.jpg'
+        });
     });
-});
+}
 
